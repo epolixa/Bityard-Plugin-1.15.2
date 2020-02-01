caa186e0b983b08578bdb55d585a387e2994b7ea
epo <epolixa@gmail.com>
2020-02-01 12:41:55 -0500

    copy from old project

---
+++ src/com/epolixa/bityard/Bityard.java
@@ -0,0 +1,51 @@
+package com.epolixa.bityard;
+
+import org.bukkit.Location;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Bityard extends JavaPlugin
+{
+    private static AFK afk;
+
+    // Startup
+    @Override
+    public void onEnable()
+    {
+        log("Enabling...");
+
+        // Register orphan event listeners
+        PluginManager pluginManager = getServer().getPluginManager();
+        pluginManager.registerEvents(new BeaconListener(this), this); // beacon titles
+        pluginManager.registerEvents(new MOTDListener(this), this); // change motd from town hall
+        pluginManager.registerEvents(new ElytraListener(this), this); // elytra polish
+
+        // Start child classes
+        log("Starting child classes");
+        afk = new AFK(this); afk.start(); // AFK
+
+        log("Enabled");
+    }
+
+    // Shutdown
+    @Override
+    public void onDisable()
+    {
+        log("Disabling...");
+
+        // Stop child classes
+        log("Stopping child classes");
+        afk.stop(); // AFK
+
+        log("Disabled");
+    }
+
+    public void log(String msg) {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        getLogger().info("[" + stackTraceElements[2].getClassName() + "] : " + msg);
+    }
+
+    public String locationXYZ(Location loc) {
+        return loc.getBlockX() + "x, " + loc.getBlockY() + "y, " + loc.getBlockZ() + "z";
+    }
+}
---
+++ src/com/epolixa/bityard/MOTDListener.java
@@ -0,0 +1,56 @@
+package com.epolixa.bityard;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+import java.util.Random;
+
+public class MOTDListener implements Listener
+{
+    private final Bityard bityard;
+    private Location cornerA;
+    private Location cornerB;
+
+    public MOTDListener(Bityard bityard)
+    {
+        this.bityard = bityard;
+        FileConfiguration config =  bityard.getConfig();
+        this.cornerA = new Location(bityard.getServer().getWorld("world"), config.getInt("MOTD_X_MIN"), config.getInt("MOTD_Y_MIN"), config.getInt("MOTD_Z_MIN"));
+        this.cornerB = new Location(bityard.getServer().getWorld("world"), config.getInt("MOTD_X_MAX"), config.getInt("MOTD_Y_MAX"), config.getInt("MOTD_Z_MAX"));
+    }
+
+
+    @EventHandler
+    public void onMOTDUpdate(ServerListPingEvent event)
+    {
+        Sign sign = null;
+        Random random = new Random();
+
+        // pick random sign location between corners
+        Location signLoc = new Location(
+            bityard.getServer().getWorld("world"),
+            cornerA.getBlockX() == cornerB.getBlockX() ? cornerA.getBlockX() : cornerA.getBlockX() + random.nextInt(cornerB.getBlockX() - cornerA.getBlockX() + 1),
+            cornerA.getBlockY() == cornerB.getBlockY() ? cornerA.getBlockY() : cornerA.getBlockY() + random.nextInt(cornerB.getBlockY() - cornerA.getBlockY() + 1),
+            cornerA.getBlockZ() == cornerB.getBlockZ() ? cornerA.getBlockZ() : cornerA.getBlockZ() + random.nextInt(cornerB.getBlockZ() - cornerA.getBlockZ() + 1)
+        );
+
+        if (bityard.getServer().getWorld("world").getBlockAt(signLoc).getType() == Material.WALL_SIGN)
+        {
+            sign = (Sign)bityard.getServer().getWorld("world").getBlockAt(signLoc).getState();
+        }
+        if (sign != null)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append(sign.getLine(0));
+            for (int i = 1; i < sign.getLines().length; i++)
+                stringBuilder.append(" " + sign.getLine(i));
+            String signText = stringBuilder.toString();
+            event.setMotd(signText);
+        }
+    }
+}
